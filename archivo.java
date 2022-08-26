e37a7eccb8d69d8cec2fea1b83d656c4ac241748
Francisco Porras <francisco.sofwareengineer04@gmail.com>
2022-08-26 00:23:04 -0500

    mostrando status corto

--- archivo.java
+++ archivo.java
@@ -7,5 +7,8 @@ public class archivo {
         public static void main(String[] args) {
                 System.out.println("hola pichurria");
                 System.out.println(" abrite loca");
+                System.out.println(" what happen");
         }
+        
+        
 }
