e77bb1cd564973b52fe34817488d2ec7e5819001
Francisco Porras <francisco.sofwareengineer04@gmail.com>
2022-08-25 23:58:19 -0500

    Agregando respuesta al hola

--- archivo.java
+++ archivo.java
@@ -6,5 +6,6 @@ public class archivo {
         
         public static void main(String[] args) {
                 System.out.println("hola pichurria");
+                System.out.println(" abrite loca");
         }
 }
