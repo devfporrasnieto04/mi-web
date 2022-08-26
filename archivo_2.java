ebe07d4bb9ca5ec94357a4a4b402bf26f7194563
Francisco Porras <francisco.sofwareengineer04@gmail.com>
2022-08-26 00:34:54 -0500

    actualizando archivo 2

--- archivo_2.java
+++ archivo_2.java
@@ -1,5 +1,6 @@
 public class archivo_2 {
         public static void main(String[] args) {
          System.out.println(" saludos locas");
+         System.out.println(" saludos locas por mil");
         }
 }
